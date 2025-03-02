00f78957a441cc4631675bb0ec9707f49f25eff0
ricardosvale <ricardost3@gmail.com>
2025-03-01 23:32:49 -0300

    Location

--- RickAndMortySwiftUI/Sources/Views/Location/LocationView.swift
+++ RickAndMortySwiftUI/Sources/Views/Location/LocationView.swift
@@ -9,30 +9,21 @@ import SwiftUI
 
 struct LocationView: View {
     var body: some View {
-        Text("Locais")
-            .font(.jockeyOne(size: 36))
-        Text("Explore o universo")
-            .font(.jost(.regular, size: 20))
-        
-        ZStack {
-            Image("locationImg")
-                .resizable()
-                .cornerRadius(10)
-                .clipped()
-                .frame(width: 360, height: 120)
-           
-            
-            VStack (alignment: .leading) {
-                Text("Name")
-                    .font(.instrument(.bold, size: 24))
-                    .foregroundStyle(.colorLabel)
-                Text("Type")
-                    .font(.instrument(.medium, size: 16))
-                    .foregroundStyle(.colorLabel)
+        VStack (alignment: .leading) {
+            Text("Locais")
+                .font(.jockeyOne(size: 36))
+            Text("Explore o universo")
+                .font(.jost(.regular, size: 20))
+        }
+        .padding(.leading, -180)
+        VStack{
+            ForEach(0..<5) { _ in
+                CardLocationView()
             }
-            .padding(.leading, -160)
-         }
+        }
         .padding()
+        
+        Spacer()
     }
 }
 
