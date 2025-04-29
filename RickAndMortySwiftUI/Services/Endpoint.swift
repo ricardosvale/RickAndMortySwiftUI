6f2c145d7b49261357c310ca814a9bff2ac7c017
ricardosvale <ricardost3@gmail.com>
2025-04-29 10:57:49 -0300

    Endpoint
    Separação dos endpoints

---
+++ RickAndMortySwiftUI/Services/Endpoint.swift
@@ -0,0 +1,15 @@
+//
+//  Endpoint.swift
+//  RickAndMortySwiftUI
+//
+//  Created by Ricardo Silva Vale on 29/04/25.
+//
+
+import Foundation
+
+enum Endpoint {
+    static let base = "https://rickandmortyapi.com/api/"
+    static let characters = base + "character"
+    static let locations = base + "location"
+    static let episodes = base + "episode"
+}
