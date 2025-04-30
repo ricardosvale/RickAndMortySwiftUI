fc0b787785e0da3ab7d212648e813c3541a05b7d
ricardosvale <ricardost3@gmail.com>
2025-04-30 10:52:36 -0300

    Feature; View dos Personagens

--- RickAndMortySwiftUI/Sources/Views/Characters/CharactersView.swift
+++ RickAndMortySwiftUI/Sources/Views/Characters/CharactersView.swift
@@ -1,16 +1,48 @@
+//
+//  CharactersViewModel.swift
+//  RickAndMortySwiftUI
+//
+//  Created by Ricardo Silva Vale on 11/11/24.
+//
 import SwiftUI
 
 struct CharactersView: View {
-
+    @StateObject private var viewModel = CharacterViewModel()
+    
     let columns: [GridItem] = [
         GridItem(.flexible()),
         GridItem(.flexible())
     ]
-
+    
     var body: some View {
-        Text("teste")
-        Text("teste")
-
+        ZStack {
+            Background()
+            
+            VStack(alignment: .leading) {
+                SearchBar()
+                    .padding(.top, 30)
+                    .padding(.horizontal)
+              
+                if viewModel.isLoading {
+                    ProgressView("Carregando...")
+                } else if let errorMessage = viewModel.errorMessage {
+                    Text(errorMessage)
+                        .foregroundColor(.red)
+                } else {
+                    ScrollView {
+                        LazyVGrid(columns: columns){
+                            ForEach(viewModel.characters) { character in
+                                CardCharacters(character: character)
+                            }
+                        }
+                    }
+                }
+               Spacer()
+            }
+            .onAppear {
+            viewModel.fetchCharactersData()
+            }
+        }
     }
 }
 
