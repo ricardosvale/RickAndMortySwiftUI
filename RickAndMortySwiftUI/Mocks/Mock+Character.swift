06df806e88308a8bd0aeba99a541e06b10c5a0ad
ricardosvale <ricardost3@gmail.com>
2025-06-05 11:05:57 -0300

    HorizontalLIst
    Adiconado os componente que lista os personagens

--- RickAndMortySwiftUI/Mocks/Mock+Character.swift
+++ RickAndMortySwiftUI/Mocks/Mock+Character.swift
@@ -19,4 +19,33 @@ extension Character {
                   image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
                   episode: ["https://rickandmortyapi.com/api/episode/1", "https://rickandmortyapi.com/api/episode/2"])
     }
+    
+    static func characterMock1() -> [Character] {
+        [Character(id: 1,
+                   name: "Rick Sanchez",
+                   status: "Alive",
+                   species: "Human",
+                   gender: "Male",
+                   origin: Origin(name: "Earth", url: ""),
+                   location: LocationCharacter(name: "Citadel of Ricks", url: ""),
+                   image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+                   episode: ["https://rickandmortyapi.com/api/episode/1", "https://rickandmortyapi.com/api/episode/2"]),
+         Character(id:2,
+                     name: "Morty Smith",
+                     status: "Alive",
+                     species: "Human",
+                     gender: "Male",
+                     origin:Origin(
+                         name: "unknown",
+                         url: ""),
+                     location: LocationCharacter(
+                         name: "Citadel of Ricks",
+                         url: "https://rickandmortyapi.com/api/location/3"
+                     ),
+                     image: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+                     episode: [
+                         "https://rickandmortyapi.com/api/episode/1",
+                         "https://rickandmortyapi.com/api/episode/2"])
+                 ]
+    }
 }
