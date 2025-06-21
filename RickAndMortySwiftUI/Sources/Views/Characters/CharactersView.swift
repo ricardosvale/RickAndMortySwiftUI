4597e79bc151007119614747eb766a177a0f1bdb
ricardosvale <ricardost3@gmail.com>
2025-06-20 22:56:17 -0300

    Navegação Home

--- RickAndMortySwiftUI/Sources/Views/Characters/CharactersView.swift
+++ RickAndMortySwiftUI/Sources/Views/Characters/CharactersView.swift
@@ -7,7 +7,7 @@
 import SwiftUI
 
 struct CharactersView: View {
-    @StateObject private var viewModel = CharacterViewModel()
+    @ObservedObject private var viewModel = CharacterViewModel()
     @State private var searchText = ""
     @FocusState private var isSearchFocused: Bool
     
@@ -21,48 +21,47 @@ struct CharactersView: View {
             ZStack {
                 Background(color: .back)
                     .ignoresSafeArea()
-                NavigationStack {
-                    if viewModel.isLoading {
-                        ProgressView("Carregando...")
-                            .frame(width: geometry.size.width, height: geometry.size.height)
-                    } else if let errorMessage = viewModel.errorMessage {
-                        Text(errorMessage)
-                            .foregroundColor(.red)
-                            .frame(width: geometry.size.width, height: geometry.size.height)
-                    } else {
-                        ScrollView {
-                            VStack(spacing: 12) {
-                                SearchBar(search: $searchText)
-                                    .focused($isSearchFocused)
-                                    .padding(.top, 20)
-                                    .padding(.horizontal)
-                                
-                                LazyVGrid(columns: columns, spacing: 12) {
-                                    ForEach(viewModel.characters) { character in
-                                        NavigationLink(destination: CharacterDetailsView(character: character)) {
-                                            CardCharacters(character: character)
-                                        }
-                                        .buttonStyle(PlainButtonStyle())
-                                        .foregroundStyle(.primary)
-                                        .onAppear {
-                                            guard let index = viewModel.characters.firstIndex(of: character) else { return }
-                                            if index == viewModel.characters.count - 4 {
-                                                viewModel.loadMoreCharacters()
-                                            }
+                
+                if viewModel.isLoading {
+                    ProgressView("Carregando...")
+                        .frame(width: geometry.size.width, height: geometry.size.height)
+                } else if let errorMessage = viewModel.errorMessage {
+                    Text(errorMessage)
+                        .foregroundColor(.red)
+                        .frame(width: geometry.size.width, height: geometry.size.height)
+                } else {
+                    ScrollView {
+                        VStack(spacing: 12) {
+                            SearchBar(search: $searchText)
+                                .focused($isSearchFocused)
+                                .padding(.top, 20)
+                                .padding(.horizontal)
+                            
+                            LazyVGrid(columns: columns, spacing: 12) {
+                                ForEach(viewModel.characters) { character in
+                                    NavigationLink(destination: CharacterDetailsView(character: character)) {
+                                        CardCharacters(character: character)
+                                    }
+                                    .buttonStyle(PlainButtonStyle())
+                                    .foregroundStyle(.primary)
+                                    .onAppear {
+                                        guard let index = viewModel.characters.firstIndex(of: character) else { return }
+                                        if index == viewModel.characters.count - 4 {
+                                            viewModel.loadMoreCharacters()
                                         }
                                     }
                                 }
-                                .padding(.horizontal)
                             }
-                            .padding(.bottom, 100) // espaço extra pro teclado
-                            .frame(minHeight: geometry.size.height)
+                            .padding(.horizontal)
                         }
-                        .padding(.bottom, 40)
-                        .onTapGesture {
-                            isSearchFocused = false
-                        }
-                        .ignoresSafeArea(.keyboard)
+                        .padding(.bottom, 100) // espaço extra pro teclado
+                        .frame(minHeight: geometry.size.height)
                     }
+                    .padding(.bottom, 40)
+                    .onTapGesture {
+                        isSearchFocused = false
+                    }
+                    .ignoresSafeArea(.keyboard)
                 }
             }
         }
--- RickAndMortySwiftUI/Sources/Views/Home/HomeView.swift
+++ RickAndMortySwiftUI/Sources/Views/Home/HomeView.swift
@@ -8,7 +8,8 @@
 import SwiftUI
 
 struct HomeView: View {
-
+    @EnvironmentObject var router: Router
+    @StateObject private var characterViewModel = CharacterViewModel()
     var body: some View {
         ZStack {
             Background(color: .back)
@@ -17,14 +18,30 @@ struct HomeView: View {
                     .font(.jockeyOne(size: 42))
                     .padding(10)
                     .padding(.top, 30)
-
+                
                 CardNavigationHome(title: "Personagens", description: "Detalhes sobre o personagem")
-
+                    .onTapGesture {
+                        router.push(.characters)
+                    }
                 CardNavigationHome(title: "Universo", description: "Explore os mundos dentro do universo Rick and Morty")
-
+                    .onTapGesture {
+                        router.push(.universe)
+                    }
                 CardNavigationHome(title: "Episódios", description: "Todos os episódios da série com detalhes")
-
+                    .onTapGesture {
+                        router.push(.episodies)
+                    }
                 Spacer()
+            }
+           .navigationDestination(for: Route.self) { route in
+                switch route {
+                case .characters:
+                    CharactersView()
+                case .universe:
+                    LocationView()
+                case .episodies:
+                    EpisodeView()
+                }
             }
         }
     }
